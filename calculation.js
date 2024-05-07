6f8658fb60f8b3766237a946ceb05fb81dd85fc3
aedan <113115936+FRHSDaedan@users.noreply.github.com>
2024-05-07 13:41:23 -0400

    CRAZYYY CHANGES
    table, more functions even though half of them dont work

--- calculation.js
+++ calculation.js
@@ -7,12 +7,49 @@ function clearScreen() {
 }
 
 function calculate() {
-    let p = document.getElementById("result").value;
+    let p = document.getElementById("display").value;
     let q = eval(p);
-    document.getElementById("result").value = q;
+    document.getElementById("display").value = q;
+
+    try {
+        // Convert trigonometric function inputs from degrees to radians
+        expression = expression.replace(/sin\(/g, 'sin(' + Math.PI / 180 + '*');
+        expression = expression.replace(/cos\(/g, 'cos(' + Math.PI / 180 + '*');
+        expression = expression.replace(/tan\(/g, 'tan(' + Math.PI / 180 + '*');
+
+        result = math.evaluate(expression);
+        display.value = result;
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+function squareRoot() {
+    let display = document.getElementById("display");
+    display.value += "sqrt(";
+}
+
+function base10Log() {
+    let display = document.getElementById("display");
+    display.value += "log(";
+}
+
+function pi() {
+    let display = document.getElementById("display");
+    display.value += "pi";
+}
+
+function e() {
+    let display = document.getElementById("display");
+    display.value += "e";
+}
+
+function power() {
+    let display = document.getElementById("display");
+    display.value += "**";
 }
 
 function backSpace() {
-    let bsp = document.getElementById("result").value;
-    document.getElementById("result").value=bsp.substring(0,bsp.length -1);
+    let bsp = document.getElementById("display").value;
+    document.getElementById("display").value=bsp.substring(0,bsp.length -1);
 }
